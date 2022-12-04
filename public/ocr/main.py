80ad5da270b40d40d57f5bbff00b3818fc85b19a
FajarAziziBM <fajaraziziboangmanlu97902@gmail.com>
2022-12-04 17:18:04 +0700

    menambahkan filterisasi transaksi bank bsi or no

--- public/ocr/main.py
+++ public/ocr/main.py
@@ -27,181 +27,192 @@ def api():
     gambar = base64.b64decode(res[1])
     img = Image.open(io.BytesIO(gambar))
     output = dict()
-    # image = img[175:220, 75:640]
-    image = img.crop((75,175,640,220))
+    
+    image = img.crop((120,45, 300,150))
 
-    d = pytesseract.image_to_string(image)
+    a = pytesseract.image_to_string(image)
 
-    for b in d.splitlines():
-        b = b.split(' ')
+    b = dict()
+    b['bank'] = a
+    b['bank'] = b['bank'].replace('\n', ' ')
 
-    d = b[0] + " " + b[1]
+    if(b['bank'] == "BANK SYARIAH INDONESIA "):
+        image = img.crop((75,175,640,220))
+        d = pytesseract.image_to_string(image)
 
-    if (d == "Transfer Bank" or d == "Transfer Antar"):
+        for b in d.splitlines():
+            b = b.split(' ')
 
-        # image = img[250:850, 75:640]
-        image = img.crop((75,250,640,850))
-        roi = [
-            [(5, (11 + 23)), (82, 640), 'status', 'text'],
-            [(66, (71 + 23)), (200, 640), 'nomor_transaksi', 'text'],
-            [(95, (100 + 28)), (206, 640), 'tgl', 'tanggal'],
-            [(156, (161 + 23)), (155, 640), 'nomor_struk', 'text'],
-            [(245, (250 + 28)), (108, 640), 'pengirim', 'text'],
-            [(336, (341 + 23)), (150, 640), 'no_rekening_tujuan', 'text'],
-            [(365, (370 + 24)), (179, 640), 'bank_penerima', 'text'],
-            [(395, (400 + 24)), (120, 640), 'nama_penerima', 'text'],
-            [(455, (460 + 24)), (126, 640), 'jumlah', 'number']
-        ]
+        d = b[0] + " " + b[1]
 
-        for x, r in enumerate(roi):
-            imgCrop = image.crop((r[1][0],r[0][0], r[1][1],r[0][1]))
+        if (d == "Transfer Bank" or d == "Transfer Antar"):
 
-            text = pytesseract.image_to_string(imgCrop)
+            # image = img[250:850, 75:640]
+            image = img.crop((75,250,640,850))
+            roi = [
+                [(5, (11 + 23)), (82, 640), 'status', 'text'],
+                [(66, (71 + 23)), (200, 640), 'nomor_transaksi', 'text'],
+                [(95, (100 + 28)), (206, 640), 'tgl', 'tanggal'],
+                [(156, (161 + 23)), (155, 640), 'nomor_struk', 'text'],
+                [(245, (250 + 28)), (108, 640), 'pengirim', 'text'],
+                [(336, (341 + 23)), (150, 640), 'no_rekening_tujuan', 'text'],
+                [(365, (370 + 24)), (179, 640), 'bank_penerima', 'text'],
+                [(395, (400 + 24)), (120, 640), 'nama_penerima', 'text'],
+                [(455, (460 + 24)), (126, 640), 'jumlah', 'number']
+            ]
 
-            if r[3] == 'text':
-                output[r[2]] = text.replace("\n", "")
-            if r[3] == 'number':
-                output[r[2]] = int(((text.replace("\n", "")).replace(".", "")).replace(",", ""))
-            if r[3] == 'tanggal':
-                text = datetime.strptime(((text.replace("\n", "")).replace(" a", "")), "%d %b %Y %H:%M:%S")
-                output[r[2]] = text
+            for x, r in enumerate(roi):
+                imgCrop = image.crop((r[1][0],r[0][0], r[1][1],r[0][1]))
 
-        return jsonify(output)
+                text = pytesseract.image_to_string(imgCrop)
 
-    elif (d == "Pembayaran/Pembelian ShopeePay"):
+                if r[3] == 'text':
+                    output[r[2]] = text.replace("\n", "")
+                if r[3] == 'number':
+                    output[r[2]] = int(((text.replace("\n", "")).replace(".", "")).replace(",", ""))
+                if r[3] == 'tanggal':
+                    text = datetime.strptime(((text.replace("\n", "")).replace(" a", "")), "%d %b %Y %H:%M:%S")
+                    output[r[2]] = text
 
-        # image = img[250:850, 75:640]
-        image = img.crop((75,250,640,850))
+            return jsonify(output)
 
-        roi = [
-            [(10, (16 + 28)), (90, 640), 'status', 'text'],
-            [(79, (84 + 26)), (218, 640), 'no_transaksi', 'text'],
-            [(113, (118 + 26)), (222, 640), 'tgl', 'tanggal'],
-            [(181, (186 + 26)), (170, 640), 'no_struk', 'text'],
-            [(283, (288 + 26)), (168, 640), 'jns_pembelian', 'text'],
-            [(317, (322 + 26)), (239, 640), 'no_hp', 'text'],
-            [(385, (390 + 26)), (168, 640), 'reference', 'text'],
-            [(419, (424 + 26)), (144, 640), 'nominal', 'number'],
-            [(453, (458 + 26)), (160, 640), 'fee', 'number'],
-            [(487, (492 + 26)), (220, 640), 'total', 'number']
-        ]
+        elif (d == "Pembayaran/Pembelian ShopeePay"):
 
-        for x, r in enumerate(roi):
-            imgCrop = image.crop((r[1][0],r[0][0],r[1][1],r[0][1]))
+            # image = img[250:850, 75:640]
+            image = img.crop((75,250,640,850))
 
-            text = pytesseract.image_to_string(imgCrop)
+            roi = [
+                [(10, (16 + 28)), (90, 640), 'status', 'text'],
+                [(79, (84 + 26)), (218, 640), 'no_transaksi', 'text'],
+                [(113, (118 + 26)), (222, 640), 'tgl', 'tanggal'],
+                [(181, (186 + 26)), (170, 640), 'no_struk', 'text'],
+                [(283, (288 + 26)), (168, 640), 'jns_pembelian', 'text'],
+                [(317, (322 + 26)), (239, 640), 'no_hp', 'text'],
+                [(385, (390 + 26)), (168, 640), 'reference', 'text'],
+                [(419, (424 + 26)), (144, 640), 'nominal', 'number'],
+                [(453, (458 + 26)), (160, 640), 'fee', 'number'],
+                [(487, (492 + 26)), (220, 640), 'total', 'number']
+            ]
 
-            if r[3] == 'text':
-                output[r[2]] = text.replace("\n", "")
-            if r[3] == 'number':
-                output[r[2]] = int(((text.replace("\n", "")).replace(".", "")).replace(",", "").replace("Rp", ""))
-            if r[3] == 'tanggal':
-                data = text.replace(";","")
-                text = data.replace("\n","")
-                text = datetime.strptime((text), "%d %b%Y%H:%M:%S")
-                output[r[2]] = text
+            for x, r in enumerate(roi):
+                imgCrop = image.crop((r[1][0],r[0][0],r[1][1],r[0][1]))
 
-        return jsonify(output)
+                text = pytesseract.image_to_string(imgCrop)
 
-    elif (d == "Pembayaran/Pembelian OVO"):
+                if r[3] == 'text':
+                    output[r[2]] = text.replace("\n", "")
+                if r[3] == 'number':
+                    output[r[2]] = int(((text.replace("\n", "")).replace(".", "")).replace(",", "").replace("Rp", ""))
+                if r[3] == 'tanggal':
+                    data = text.replace(";","")
+                    text = data.replace("\n","")
+                    text = datetime.strptime((text), "%d %b%Y%H:%M:%S")
+                    output[r[2]] = text
 
-        # image = img[250:850, 75:640]
-        image = img.crop((75,250,640,850))
+            return jsonify(output)
 
-        roi = [
-            [(5, (11 + 23)), (82, 640), 'status', 'text'],
-            [(66, (71 + 23)), (200, 640), 'nomor_transaksi', 'text'],
-            [(95, (100 + 28)), (206, 640), 'tgl', 'tanggal'],
-            [(156, (161 + 23)), (155, 640), 'nomor_struk', 'text'],
-            [(245, (250 + 28)), (150, 640), 'jenis_pembelian', 'text'],
-            [(280, (285 + 23)), (210, 640), 'no_pelanggan', 'text'],
-            [(310, (315 + 23)), (245, 640), 'nama_pelanggan', 'text'],
-            [(336, (341 + 26)), (165, 640), 'nilai_topup', 'number'],
-            [(395, (400 + 24)), (238, 640), 'total_bayar', 'number']
-        ]
+        elif (d == "Pembayaran/Pembelian OVO"):
 
-        for x, r in enumerate(roi):
-            imgCrop = image.crop((r[1][0],r[0][0], r[1][1],r[0][1]))
+            # image = img[250:850, 75:640]
+            image = img.crop((75,250,640,850))
 
-            text = pytesseract.image_to_string(imgCrop)
+            roi = [
+                [(5, (11 + 23)), (82, 640), 'status', 'text'],
+                [(66, (71 + 23)), (200, 640), 'nomor_transaksi', 'text'],
+                [(95, (100 + 28)), (206, 640), 'tgl', 'tanggal'],
+                [(156, (161 + 23)), (155, 640), 'nomor_struk', 'text'],
+                [(245, (250 + 28)), (150, 640), 'jenis_pembelian', 'text'],
+                [(280, (285 + 23)), (210, 640), 'no_pelanggan', 'text'],
+                [(310, (315 + 23)), (245, 640), 'nama_pelanggan', 'text'],
+                [(336, (341 + 26)), (165, 640), 'nilai_topup', 'number'],
+                [(395, (400 + 24)), (238, 640), 'total_bayar', 'number']
+            ]
 
-            if r[3] == 'text':
-                output[r[2]] = text.replace("\n", "")
-            if r[3] == 'number':
-                output[r[2]] = int(((text.replace("\n", "")).replace(".", "")).replace(",", "").replace("Rp", ""))
-            if r[3] == 'tanggal':
-                text = datetime.strptime(((text.replace("\n", "")).replace(" a", "")), "%d %b %Y %H:%M:%S")
-                output[r[2]] = text
+            for x, r in enumerate(roi):
+                imgCrop = image.crop((r[1][0],r[0][0], r[1][1],r[0][1]))
 
-        return jsonify(output)
+                text = pytesseract.image_to_string(imgCrop)
 
-    elif (d == "Pembayaran/Pembelian Go"):
+                if r[3] == 'text':
+                    output[r[2]] = text.replace("\n", "")
+                if r[3] == 'number':
+                    output[r[2]] = int(((text.replace("\n", "")).replace(".", "")).replace(",", "").replace("Rp", ""))
+                if r[3] == 'tanggal':
+                    text = datetime.strptime(((text.replace("\n", "")).replace(" a", "")), "%d %b %Y %H:%M:%S")
+                    output[r[2]] = text
 
-        # image = img[250:850, 75:640]
-        image = img.crop((75,250,640,850))
+            return jsonify(output)
 
-        roi = [
-            [(5, (11 + 23)), (82, 640), 'status', 'text'],
-            [(66, (71 + 23)), (200, 640), 'nomor_transaksi', 'text'],
-            [(95, (100 + 28)), (206, 640), 'tgl', 'tanggal'],
-            [(156, (161 + 23)), (155, 640), 'nomor_struk', 'text'],
-            [(245, (250 + 28)), (180, 640), 'jenis_pembayaran', 'text'],
-            [(280, (285 + 23)), (219, 640), 'no_handphone', 'text'],
-            [(310, (315 + 23)), (230, 640), 'nama_costumer', 'text'],
-            [(336, (341 + 23)), (150, 640), 'admin_fee', 'number'],
-            [(365, (370 + 24)), (220, 640), 'nominal_topup', 'number'],
-            [(395, (400 + 24)), (120, 640), 'no_reff', 'text'],
-            [(427, (431 + 24)), (195, 640), 'total_bayar', 'number']
-        ]
+        elif (d == "Pembayaran/Pembelian Go"):
 
-        for x, r in enumerate(roi):
-            imgCrop = image.crop((r[1][0],r[0][0], r[1][1],r[0][1]))
+            # image = img[250:850, 75:640]
+            image = img.crop((75,250,640,850))
 
-            text = pytesseract.image_to_string(imgCrop)
+            roi = [
+                [(5, (11 + 23)), (82, 640), 'status', 'text'],
+                [(66, (71 + 23)), (200, 640), 'nomor_transaksi', 'text'],
+                [(95, (100 + 28)), (206, 640), 'tgl', 'tanggal'],
+                [(156, (161 + 23)), (155, 640), 'nomor_struk', 'text'],
+                [(245, (250 + 28)), (180, 640), 'jenis_pembayaran', 'text'],
+                [(280, (285 + 23)), (219, 640), 'no_handphone', 'text'],
+                [(310, (315 + 23)), (230, 640), 'nama_costumer', 'text'],
+                [(336, (341 + 23)), (150, 640), 'admin_fee', 'number'],
+                [(365, (370 + 24)), (220, 640), 'nominal_topup', 'number'],
+                [(395, (400 + 24)), (120, 640), 'no_reff', 'text'],
+                [(427, (431 + 24)), (195, 640), 'total_bayar', 'number']
+            ]
 
-            if r[3] == 'text':
-                output[r[2]] = text.replace("\n", "")
-            if r[3] == 'number':
-                output[r[2]] = int(((text.replace("\n", "")).replace(".", "")).replace(",", "").replace("Rp", ""))
-            if r[3] == 'tanggal':
-                text = datetime.strptime(((text.replace("\n", "")).replace(" a", "")), "%d %b %Y %H:%M:%S")
-                output[r[2]] = text
+            for x, r in enumerate(roi):
+                imgCrop = image.crop((r[1][0],r[0][0], r[1][1],r[0][1]))
 
-        return jsonify(output)
+                text = pytesseract.image_to_string(imgCrop)
 
-    elif (d == "Pembayaran/Pembelian DANA"):
+                if r[3] == 'text':
+                    output[r[2]] = text.replace("\n", "")
+                if r[3] == 'number':
+                    output[r[2]] = int(((text.replace("\n", "")).replace(".", "")).replace(",", "").replace("Rp", ""))
+                if r[3] == 'tanggal':
+                    text = datetime.strptime(((text.replace("\n", "")).replace(" a", "")), "%d %b %Y %H:%M:%S")
+                    output[r[2]] = text
 
-        # image = img[250:850, 75:640]
-        image = img.crop((75,250,640,850))
+            return jsonify(output)
 
-        roi = [
-            [(6, (11 + 23)), (82, 640), 'status', 'text'],
-            [(66, (71 + 23)), (201, 560), 'no_transaksi', 'text'],
-            [(90, (100 + 28)), (204, 560), 'tgl', 'tanggal'],
-            [(155, (160 + 24)), (154, 560), 'no_struk', 'text'],
-            [(246, (251 + 23)), (157, 640), 'jns_pembelian', 'text'],
-            [(276, (281 + 23)), (90, 640), 'no_hp', 'text'],
-            [(306, (311 + 23)), (243, 560), 'nama', 'text'],
-            [(336, (341 + 25)), (170, 640), 'jumlah', 'number'],
-            [(396, (401 + 23)), (0, 640), 'order_id', 'text'],
-            [(425, (430 + 24)), (175, 640), 'transaksi_id', 'text'],
-            [(456, (461 + 25)), (199, 560), 'total', 'number']
+        elif (d == "Pembayaran/Pembelian DANA"):
 
-        ]
+            # image = img[250:850, 75:640]
+            image = img.crop((75,250,640,850))
 
-        for x, r in enumerate(roi):
-            imgCrop = image.crop((r[1][0],r[0][0], r[1][1],r[0][1]))
+            roi = [
+                [(6, (11 + 23)), (82, 640), 'status', 'text'],
+                [(66, (71 + 23)), (201, 560), 'no_transaksi', 'text'],
+                [(90, (100 + 28)), (204, 560), 'tgl', 'tanggal'],
+                [(155, (160 + 24)), (154, 560), 'no_struk', 'text'],
+                [(246, (251 + 23)), (157, 640), 'jns_pembelian', 'text'],
+                [(276, (281 + 23)), (90, 640), 'no_hp', 'text'],
+                [(306, (311 + 23)), (243, 560), 'nama', 'text'],
+                [(336, (341 + 25)), (170, 640), 'jumlah', 'number'],
+                [(396, (401 + 23)), (0, 640), 'order_id', 'text'],
+                [(425, (430 + 24)), (175, 640), 'transaksi_id', 'text'],
+                [(456, (461 + 25)), (199, 560), 'total', 'number']
 
-            text = pytesseract.image_to_string(imgCrop)
+            ]
 
-            if r[3] == 'text':
-                output[r[2]] = text.replace("\n", "")
-            if r[3] == 'number':
-                output[r[2]] = int(((text.replace("\n", "")).replace(".", "")).replace(",", "").replace("Rp", ""))
-            if r[3] == 'tanggal':
-                text = datetime.strptime(((text.replace("\n", "")).replace(" a", "")), "%d %b %Y %H:%M:%S")
-                output[r[2]] = text
+            for x, r in enumerate(roi):
+                imgCrop = image.crop((r[1][0],r[0][0], r[1][1],r[0][1]))
 
-        return jsonify(output)
+                text = pytesseract.image_to_string(imgCrop)
+
+                if r[3] == 'text':
+                    output[r[2]] = text.replace("\n", "")
+                if r[3] == 'number':
+                    output[r[2]] = int(((text.replace("\n", "")).replace(".", "")).replace(",", "").replace("Rp", ""))
+                if r[3] == 'tanggal':
+                    text = datetime.strptime(((text.replace("\n", "")).replace(" a", "")), "%d %b %Y %H:%M:%S")
+                    output[r[2]] = text    
+            return jsonify(output)
+
+    else : print("Permintaan ditolak, keluar dari proses!")
+
+    return jsonify(b['bank'])
 
 app.run(debug=True)
--- resources/views/form.blade.php
+++ resources/views/form.blade.php
@@ -1,63 +1,54 @@
 @extends('layout.main')
 @section('container')
-    <div class="card" style="margin: 10px 200px">
-        <div class="card-header">
-        </div>
-        <div class="card-body">
-            <form action="">
-                <div class="mb-3">
-                    <label for="exampleFormControlInput1" class="form-label">Status</label>
-                    <input type="text" class="form-control" id="status" placeholder="status" name="status"
-                        value="{{ $data['status'] }}">
-                </div>
-                <div class="mb-3">
-                    <label for="exampleFormControlInput1" class="form-label">Nomor Transaksi</label>
-                    <input type="text" class="form-control" id="no_transaksi" placeholder="no_transaksi"
-                        name="no_transaksi" value="{{ $data['nomor_transaksi'] }}">
-                </div>
-                <div class="mb-3">
-                    @php
-                        $data['tgl'] = date('Y-m-d H:i', strtotime($data['tgl']));
-                    @endphp
-                    <label for="exampleFormControlInput1" class="form-label">Tanggal dan Waktu</label>
-                    <input type="datetime-local" class="form-control" id="tgl" placeholder="tgl" name="tgl"
-                        value="{{ $data['tgl'] }}">
-                </div>
-                <div class="mb-3">
-                    <label for="exampleFormControlInput1" class="form-label">Nomor Struk</label>
-                    <input type="text" class="form-control" id="no_struk" placeholder="no_struk" name="no_struk"
-                        value="{{ $data['nomor_struk'] }}">
-                </div>
-                <div class="mb-3">
-                    <label for="exampleFormControlInput1" class="form-label">Nama Pengirim</label>
-                    <input type="text" class="form-control" id="pengirim" placeholder="pengirim" name="pengirim"
-                        value="{{ $data['pengirim'] }}">
-                </div>
-                <div class="mb-3">
-                    <label for="exampleFormControlInput1" class="form-label">Nomor Rekening Tujuan</label>
-                    <input type="text" class="form-control" id="no_rekening_tujuan" placeholder="no_rekening_tujuan"
-                        name="no_rekening_tujuan" value="{{ $data['no_rekening_tujuan'] }}">
-                </div>
-                <div class="mb-3">
-                    <label for="exampleFormControlInput1" class="form-label">Bank Penerima</label>
-                    <input type="text" class="form-control" id="bank_penerima" placeholder="bank_penerima"
-                        name="bank_penerima" value="{{ $data['bank_penerima'] }}">
-                </div>
-                <div class="mb-3">
-                    <label for="exampleFormControlInput1" class="form-label">Nama Penerima</label>
-                    <input type="text" class="form-control" id="nama_penerima" placeholder="nama_penerima"
-                        name="nama_penerima" value="{{ $data['nama_penerima'] }}">
-                </div>
-                <div class="mb-3">
-                    <label for="exampleFormControlInput1" class="form-label">Admin Fee</label>
-                    <input type="number" class="form-control" id="admin_fee" placeholder="admin_fee" name="admin_fee">
-                </div>
-                <div class="mb-3">
-                    <label for="exampleFormControlInput1" class="form-label">Total</label>
-                    <input type="number" class="form-control" id="total" placeholder="total" name="total"
-                        value="{{ $data['jumlah'] }}">
-                </div>
-            </form>
-        </div>
+<div class="card" style="margin: 10px 200px">
+    <div class="card-header">
     </div>
+    <div class="card-body">
+        <form action="">
+            <div class="mb-3">
+                <label for="exampleFormControlInput1" class="form-label">Status</label>
+                <input type="text" class="form-control" id="status" placeholder="status" name="status" value="{{ $data['status'] }}">
+            </div>
+            <div class="mb-3">
+                <label for="exampleFormControlInput1" class="form-label">Nomor Transaksi</label>
+                <input type="text" class="form-control" id="no_transaksi" placeholder="no_transaksi" name="no_transaksi" value="{{ $data['nomor_transaksi'] }}">
+            </div>
+            <div class="mb-3">
+                @php
+                $data['tgl'] = date('Y-m-d H:i', strtotime($data['tgl']));
+                @endphp
+                <label for="exampleFormControlInput1" class="form-label">Tanggal dan Waktu</label>
+                <input type="datetime-local" class="form-control" id="tgl" placeholder="tgl" name="tgl" value="{{ $data['tgl'] }}">
+            </div>
+            <div class="mb-3">
+                <label for="exampleFormControlInput1" class="form-label">Nomor Struk</label>
+                <input type="text" class="form-control" id="no_struk" placeholder="no_struk" name="no_struk" value="{{ $data['nomor_struk'] }}">
+            </div>
+            <div class="mb-3">
+                <label for="exampleFormControlInput1" class="form-label">Nama Pengirim</label>
+                <input type="text" class="form-control" id="pengirim" placeholder="pengirim" name="pengirim" value="{{ $data['pengirim'] }}">
+            </div>
+            <div class="mb-3">
+                <label for="exampleFormControlInput1" class="form-label">Nomor Rekening Tujuan</label>
+                <input type="text" class="form-control" id="no_rekening_tujuan" placeholder="no_rekening_tujuan" name="no_rekening_tujuan" value="{{ $data['no_rekening_tujuan'] }}">
+            </div>
+            <div class="mb-3">
+                <label for="exampleFormControlInput1" class="form-label">Bank Penerima</label>
+                <input type="text" class="form-control" id="bank_penerima" placeholder="bank_penerima" name="bank_penerima" value="{{ $data['bank_penerima'] }}">
+            </div>
+            <div class="mb-3">
+                <label for="exampleFormControlInput1" class="form-label">Nama Penerima</label>
+                <input type="text" class="form-control" id="nama_penerima" placeholder="nama_penerima" name="nama_penerima" value="{{ $data['nama_penerima'] }}">
+            </div>
+            <div class="mb-3">
+                <label for="exampleFormControlInput1" class="form-label">Admin Fee</label>
+                <input type="number" class="form-control" id="admin_fee" placeholder="admin_fee" name="admin_fee">
+            </div>
+            <div class="mb-3">
+                <label for="exampleFormControlInput1" class="form-label">Total</label>
+                <input type="number" class="form-control" id="total" placeholder="total" name="total" value="{{ $data['jumlah'] }}">
+            </div>
+        </form>
+    </div>
+</div>
 @endsection
